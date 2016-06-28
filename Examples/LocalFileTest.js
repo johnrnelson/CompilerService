6be8a166346897b6a56aae49f020d714a8d3d43d
John Nelson <john@tektology.com>
2016-06-28 17:44:03 +0000

    Normal Updates...

--- Examples/LocalFileTest.js
+++ Examples/LocalFileTest.js
@@ -4,21 +4,23 @@
 var CompService = require("../index");
 
 
-
+/*
+    Setup our testing paths...
+*/
 var TARGETFOLDER = '/home/ubuntu/workspace/TESTFILES/';
 var TARGETFile = 'testcomp.js';
 var TARGETFilePath = TARGETFOLDER + TARGETFile;
 
 
 
-
+//Call the service API...
 CompService.Service.API.ServeCompileFile(false,TARGETFilePath, function(err, FileType, FileContents) {
     if (err) {
         console.log('File ERROR-->' , TARGETFilePath);
         console.log(err);
     }
     else {
-        console.log(FileType);
+        console.log('The type of file:',FileType);
         console.log('CONTENTS-->',FileContents);
 
     } 
